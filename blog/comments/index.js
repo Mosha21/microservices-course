5371c9c8b9d118ddd88bfa0e9884bf8c6483ef90
Mosha21 <bernardo_mm@hotmail.com>
2023-03-21 10:27:30 -0600

    Feat: Post, Comment, Event bus, Query services

---
+++ blog/comments/index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const commentsByPostId = {};
+
+app.get("/posts/:id/comments", (req, res) => {
+  const { id } = req.params;
+  res.send(commentsByPostId[id] || []);
+});
+
+app.post("/posts/:id/comments", async (req, res) => {
+  const commentId = randomBytes(4).toString("hex");
+  const { content } = req.body;
+  const postId = req.params.id;
+
+  const comments = commentsByPostId[postId] || [];
+
+  comments.push({ id: commentId, content });
+
+  commentsByPostId[postId] = comments;
+
+  try {
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentCreated",
+      data: {
+        id: commentId,
+        content,
+        postId: req.params.id,
+      },
+    });
+  } catch (error) {}
+
+  res.status(201).send(comments);
+});
+
+app.post("/events", (req, res) => {
+  console.log("Received event: ", req.body.type);
+
+  res.send({});
+});
+
+app.listen(4001, () => {
+  console.log("Server listening on port 4001");
+});
